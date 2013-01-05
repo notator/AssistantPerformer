0a05701c34cc8b6f6472a84e6d4259f3f4ef8a29
James Ingram <j.ingram@netcologne.de>
2013-01-05 13:22:51 +0100

    Beginning to implement MIDI file downloading
    Two temporary buttons have been created at the top of the main page.
    Button 1 loads a test file from a test directory (temp midi) into
    a normal javascript array of numbers.
    Button 2 downloads the array as a MIDI file to the user's computer.
    When the downloaded file is the same as the original, delete the
    temporary buttons, directory and MIDI file, and use the download
    function to save performances recorded in the Assistant Performer.

---
+++ js/jiMIDIFile.js
@@ -0,0 +1,44 @@
+﻿/*
+ *  copyright 2013 James Ingram
+ *  http://james-ingram-act-two.de/
+ *
+ *  Code licensed under MIT
+ *  https://github.com/notator/assistant-performer/blob/master/License.md
+ *
+ *  jiMIDIFile.js
+ *  The JI_NAMESPACE.midiFile namespace containing two functions
+ *      load(midiFileURI)   // loads the midi file into an ordinary javascript array of numbers.
+ *                          // The array is at the base level in this namespace.
+ *      
+ *      download()          // downloads the array (set by load()) as a MIDI file. 
+ */
+
+JI_NAMESPACE.namespace('JI_NAMESPACE.midiFile');
+
+JI_NAMESPACE.midiFile = (function ()
+{
+    "use strict";
+
+    var midiArray = [], // the loaded and downloaded midiArray (a javascript array of numbers)
+
+    // loads the midi file at midiURL into midiArray.
+    load = function (midiURL)
+    {
+    },
+
+    // downloads the midiArray as a MIDI file to the user's computer
+    download = function ()
+    {
+    }
+
+    // public API
+    publicAPI =
+    {
+        load: load,
+        download: download
+    };
+
+    return publicAPI;
+
+} ());
+
