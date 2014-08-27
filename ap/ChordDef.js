06c5dc08fcbde9ec687833e923f8a64a0082a58c
James Ingram <j.ingram@netcologne.de>
2014-08-27 23:44:43 +0200

    Recompiled scores

--- ap/ChordDef.js
+++ ap/ChordDef.js
@@ -17,216 +17,216 @@ _AP.namespace('_AP.chordDef');
 
 _AP.chordDef = (function ()
 {
-    "use strict";
-    var
+	"use strict";
+	var
     // The argument is a string containing a list of integers separated by single spaces
     // This function returns the corresponding array of numbers.
     numberArray = function (numberList)
     {
-        var stringArray = numberList.split(' '),
+    	var stringArray = numberList.split(' '),
             len = stringArray.length,
             numArray = [],
             i;
 
-        for (i = 0; i < len; ++i)
-        {
-            numArray.push(parseInt(stringArray[i], 10));
-        }
-        return numArray;
+    	for(i = 0; i < len; ++i)
+    	{
+    		numArray.push(parseInt(stringArray[i], 10));
+    	}
+    	return numArray;
     },
 
     chordAttributes = function (chordDefNode)
     {
-        var a,
+    	var a,
             attributes = {},
             attributesLength = chordDefNode.attributes.length,
             i;
 
-        attributes.repeat = false; // default value
-        attributes.hasChordOff = true; // default value
+    	attributes.repeat = false; // default value
+    	attributes.hasChordOff = true; // default value
 
-        for (i = 0; i < attributesLength; ++i)
-        {
-            a = chordDefNode.attributes[i];
+    	for(i = 0; i < attributesLength; ++i)
+    	{
+    		a = chordDefNode.attributes[i];
 
-            // console.log(a.name + " = " + a.value);
+    		// console.log(a.name + " = " + a.value);
 
-            switch (a.name)
-            {
-                case "id":
-                    attributes.id = a.value; // a string
-                    break;
-                case "volume":
-                    attributes.volume = parseInt(a.value, 10);
-                    break;
-                case "repeat":
-                    if(a.value === "0")
-                    {
-                        attributes.repeat = false;
-                    }
-                    else if(a.value === "1")
-                    {
-                        attributes.repeat = true;
-                    }
-                    // if "repeat" does not occur, attributes.repeat will be false
-                    break;
-                case "hasChordOff":
-                    if(a.value === "0")
-                    {
-                        attributes.hasChordOff = false;
-                    }
-                    else if(a.value === "1")
-                    {
-                        attributes.hasChordOff = true;
-                    }
-                    // if "hasChordOff" does not occur, attributes.hasChordOff will be true
-                    break;
-                case "pitchWheelDeviation":
-                    attributes.pitchWheelDeviation = parseInt(a.value, 10);
-                    break;
-                case "minBasicChordMsDuration":
-                    attributes.minBasicChordMsDuration = parseInt(a.value, 10);
-                    break;
-                default:
-                    throw (">>>>>>>>>> Illegal midiChord attribute  <<<<<<<<<<");
-            }
-        }
-        // the following attributes can be undefined
-        //  volume
-        //  hasChordOff (true by default)
-        //  pitchWheelDeviation (default is 2 or unchanged)
-        //  minBasicChordMsDuration (default is 1 millisecond)
-        if (attributes.id === undefined)
-        {
-            throw ("Error: chords must have an id!");
-        }
+    		switch(a.name)
+    		{
+    			case "id":
+    				attributes.id = a.value; // a string
+    				break;
+    			case "volume":
+    				attributes.volume = parseInt(a.value, 10);
+    				break;
+    			case "repeat":
+    				if(a.value === "0")
+    				{
+    					attributes.repeat = false;
+    				}
+    				else if(a.value === "1")
+    				{
+    					attributes.repeat = true;
+    				}
+    				// if "repeat" does not occur, attributes.repeat will be false
+    				break;
+    			case "hasChordOff":
+    				if(a.value === "0")
+    				{
+    					attributes.hasChordOff = false;
+    				}
+    				else if(a.value === "1")
+    				{
+    					attributes.hasChordOff = true;
+    				}
+    				// if "hasChordOff" does not occur, attributes.hasChordOff will be true
+    				break;
+    			case "pitchWheelDeviation":
+    				attributes.pitchWheelDeviation = parseInt(a.value, 10);
+    				break;
+    			case "minBasicChordMsDuration":
+    				attributes.minBasicChordMsDuration = parseInt(a.value, 10);
+    				break;
+    			default:
+    				throw (">>>>>>>>>> Illegal midiChord attribute  <<<<<<<<<<");
+    		}
+    	}
+    	// the following attributes can be undefined
+    	//  volume
+    	//  hasChordOff (true by default)
+    	//  pitchWheelDeviation (default is 2 or unchanged)
+    	//  minBasicChordMsDuration (default is 1 millisecond)
+    	if(attributes.id === undefined)
+    	{
+    		throw ("Error: chords must have an id!");
+    	}
 
-        return attributes;
+    	return attributes;
     },
 
     basicChordsArray = function (chordDefNode)
     {
-        var basicChordsDef = chordDefNode.firstElementChild,
+    	var basicChordsDef = chordDefNode.firstElementChild,
             basicChordDef = basicChordsDef.firstElementChild,
             basicChrdsArray = [];
 
-        function getBasicChord(basicChordDef)
-        {
-            var attr,
+    	function getBasicChord(basicChordDef)
+    	{
+    		var attr,
                 basicChord = {},
                 attributesLength = basicChordDef.attributes.length,
                 i;
 
-            for (i = 0; i < attributesLength; ++i)
-            {
-                attr = basicChordDef.attributes[i];
-                // console.log(attr.name + " = " + attr.value);
-                switch (attr.name)
-                {
-                    case "msDuration":
-                        basicChord.msDuration = parseInt(attr.value, 10);
-                        break;
-                    case "bank":
-                        basicChord.bank = parseInt(attr.value, 10);
-                        break;
-                    case "patch":
-                        basicChord.patch = parseInt(attr.value, 10);
-                        break;
-                    case "hasChordOff":
-                        if (attr.value === "0")
-                        {
-                            basicChord.hasChordOff = false;
-                        }
-                        // is true if undefined
-                        break;
-                    case "notes":
-                        basicChord.notes = [];
-                        basicChord.notes = numberArray(attr.value);
-                        break;
-                    case "velocities":
-                        basicChord.velocities = [];
-                        basicChord.velocities = numberArray(attr.value);
-                        break;
-                    default:
-                        throw (">>>>>>>>>> Illegal basicChord attribute <<<<<<<<<<");
-                }
-            }
+    		for(i = 0; i < attributesLength; ++i)
+    		{
+    			attr = basicChordDef.attributes[i];
+    			// console.log(attr.name + " = " + attr.value);
+    			switch(attr.name)
+    			{
+    				case "msDuration":
+    					basicChord.msDuration = parseInt(attr.value, 10);
+    					break;
+    				case "bank":
+    					basicChord.bank = parseInt(attr.value, 10);
+    					break;
+    				case "patch":
+    					basicChord.patch = parseInt(attr.value, 10);
+    					break;
+    				case "hasChordOff":
+    					if(attr.value === "0")
+    					{
+    						basicChord.hasChordOff = false;
+    					}
+    					// is true if undefined
+    					break;
+    				case "notes":
+    					basicChord.notes = [];
+    					basicChord.notes = numberArray(attr.value);
+    					break;
+    				case "velocities":
+    					basicChord.velocities = [];
+    					basicChord.velocities = numberArray(attr.value);
+    					break;
+    				default:
+    					throw (">>>>>>>>>> Illegal basicChord attribute <<<<<<<<<<");
+    			}
+    		}
 
-            // the following properties can be undefined:
-            //    bank;
-            //    patch;
-            //    hasChordOff -- if undefined, is true
-            if (basicChord.msDuration === undefined
+    		// the following properties can be undefined:
+    		//    bank;
+    		//    patch;
+    		//    hasChordOff -- if undefined, is true
+    		if(basicChord.msDuration === undefined
             || basicChord.notes === undefined
             || basicChord.velocities === undefined)
-            {
-                throw ("Error: all basic chords must have msDuration, notes and velocities");
-            }
+    		{
+    			throw ("Error: all basic chords must have msDuration, notes and velocities");
+    		}
 
-            if (basicChord.notes.length !== basicChord.velocities.length)
-            {
-                throw ("Error: basic chord must have the same number of notes and velocities");
-            }
+    		if(basicChord.notes.length !== basicChord.velocities.length)
+    		{
+    			throw ("Error: basic chord must have the same number of notes and velocities");
+    		}
 
-            return basicChord;
-        }
+    		return basicChord;
+    	}
 
-        while (basicChordDef)
-        {
-            try
-            {
-                basicChrdsArray.push(getBasicChord(basicChordDef));
-                basicChordDef = basicChordDef.nextElementSibling;
-            }
-            catch (ex)
-            {
-                console.log(ex);
-            }
-        }
-        return basicChrdsArray;
+    	while(basicChordDef)
+    	{
+    		try
+    		{
+    			basicChrdsArray.push(getBasicChord(basicChordDef));
+    			basicChordDef = basicChordDef.nextElementSibling;
+    		}
+    		catch(ex)
+    		{
+    			console.log(ex);
+    		}
+    	}
+    	return basicChrdsArray;
     },
 
     // Any and all of the returned sliders properties can be undefined
     sliders = function (chordDefNode)
     {
-        var sliders = {},
+    	var sliders = {},
             attr,
             attributesLength,
             slidersDef = chordDefNode.lastElementChild,
             i;
 
-        if (slidersDef !== null)
-        {
-            attributesLength = slidersDef.attributes.length;
-            for (i = 0; i < attributesLength; ++i)
-            {
-                attr = slidersDef.attributes[i];
-                // console.log(attr.name + " = " + attr.value);
+    	if(slidersDef !== null)
+    	{
+    		attributesLength = slidersDef.attributes.length;
+    		for(i = 0; i < attributesLength; ++i)
+    		{
+    			attr = slidersDef.attributes[i];
+    			// console.log(attr.name + " = " + attr.value);
 
-                switch (attr.name)
-                {
-                    case "pitchWheel":
-                        sliders.pitchWheel = [];
-                        sliders.pitchWheel = numberArray(attr.value);
-                        break;
-                    case "pan":
-                        sliders.pan = [];
-                        sliders.pan = numberArray(attr.value);
-                        break;
-                    case "modulationWheel":
-                        sliders.modulationWheel = [];
-                        sliders.modulationWheel = numberArray(attr.value);
-                        break;
-                    case "expressionSlider":
-                        sliders.expressionSlider = [];
-                        sliders.expressionSlider = numberArray(attr.value);
-                        break;
-                    default:
-                        throw (">>>>>>>>>> Illegal slider <<<<<<<<<<");
-                }
-            }
-        }
-        return sliders;
+    			switch(attr.name)
+    			{
+    				case "pitchWheel":
+    					sliders.pitchWheel = [];
+    					sliders.pitchWheel = numberArray(attr.value);
+    					break;
+    				case "pan":
+    					sliders.pan = [];
+    					sliders.pan = numberArray(attr.value);
+    					break;
+    				case "modulationWheel":
+    					sliders.modulationWheel = [];
+    					sliders.modulationWheel = numberArray(attr.value);
+    					break;
+    				case "expressionSlider":
+    					sliders.expressionSlider = [];
+    					sliders.expressionSlider = numberArray(attr.value);
+    					break;
+    				default:
+    					throw (">>>>>>>>>> Illegal slider <<<<<<<<<<");
+    			}
+    		}
+    	}
+    	return sliders;
     },
 
     // ChordDef constructor
@@ -258,26 +258,26 @@ _AP.chordDef = (function ()
     //       chord.sliders.expressionSlider[];
     ChordDef = function (chordDefNode)
     {
-        if (!(this instanceof ChordDef))
-        {
-            return new ChordDef(chordDefNode);
-        }
+    	if(!(this instanceof ChordDef))
+    	{
+    		return new ChordDef(chordDefNode);
+    	}
 
-        this.attributes = chordAttributes(chordDefNode);
-        this.basicChordsArray = basicChordsArray(chordDefNode);
-        this.sliders = sliders(chordDefNode);
+    	this.attributes = chordAttributes(chordDefNode);
+    	this.basicChordsArray = basicChordsArray(chordDefNode);
+    	this.sliders = sliders(chordDefNode);
 
-        return this;
+    	return this;
     },
 
     // public API
     publicAPI =
     {
-        // public ChordDef(chordDefNode) constructor.
-        ChordDef: ChordDef
+    	// public ChordDef(chordDefNode) constructor.
+    	ChordDef: ChordDef
     };
 
-    return publicAPI;
+	return publicAPI;
 
-} ());
+}());
 
