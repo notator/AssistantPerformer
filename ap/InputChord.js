d190949f2d2e6b18705b0dd9b29c3e4040db81c9
notator <j.ingram@netcologne.de>
2015-02-26 15:36:40 +0100

    Bug fixes in InputChord
    InputControls were not being added to InputChords

--- ap/InputChord.js
+++ ap/InputChord.js
@@ -25,6 +25,97 @@ _AP.inputChord = (function()
     // An InputChord contains all the information required for playing an (ornamented) chord.
     InputChord = function(timeObject, outputTracks)
     {
+    	function getInputNotes(inputNoteDefs, msPosition, outputTracks)
+    	{
+    		var i, nInputNoteDefs = inputNoteDefs.length, inputNoteDef, 
+				inputNote, inputNotes = [],
+				j, trk, trks, trkMsPosition, trkRefDef, trkRefDefs,
+				trkLength,
+				k, midiObjects, indexInMidiObjects, midiObjectsAndIndex;
+
+    		function getMidiObjectsAndIndex(outputTracks, voiceID, msPosition)
+    		{
+    			var found = false, i, midiObjects, outputTrack, nOutputTracks = outputTracks.length,
+					index, midiObjectsAndIndex = {};
+
+    			for(i = 0; i < nOutputTracks; ++i)
+    			{
+    				outputTrack = outputTracks[i];
+    				if(outputTrack.voiceID === voiceID)
+    				{
+    					found = true;
+    					break;
+    				}
+    			}
+    			if(found === false)
+    			{
+    				throw "InputChord.js: Can't find the output track with the correct voiceID!";
+    			}
+
+    			found = false;
+    			midiObjects = outputTrack.midiObjects;
+    			for(index = 0; index < midiObjects.length; ++index)
+    			{
+    				if(midiObjects[index].msPositionInScore === msPosition)
+    				{
+    					found = true;
+    					break;
+    				}
+    			}
+    			if(found === false)
+    			{
+    				throw "InputChord.js: Can't find the beginning of the trk!";
+    			}
+
+    			midiObjectsAndIndex.midiObjects = midiObjects;
+    			midiObjectsAndIndex.index = index;
+    			return midiObjectsAndIndex;
+    		}
+
+    		for(i = 0; i < nInputNoteDefs; ++i)
+    		{
+    			inputNoteDef = inputNoteDefs[i];
+    			inputNote = {};
+    			inputNote.notatedKey = inputNoteDef.notatedKey;
+    			trks = [];
+    			if(inputNoteDef.inputControls !== undefined)
+    			{
+    				inputNote.inputControls = inputNoteDef.inputControls; // can be undefined
+    			}
+    			trkRefDefs = inputNoteDef.trkRefs;
+    			for(j = 0; j < trkRefDefs.length; ++j)
+    			{
+    				trkRefDef = trkRefDefs[j];
+    				trk = [];
+    				if(trkRefDef.mOffset !== undefined)
+    				{
+    					trk.msOffset = trkRefDef.mOffset; // can be undefined
+    				}
+    				if(trkRefDef.inputControls !== undefined)
+    				{
+    					trk.inputControls = trkRefDef.inputControls; // can be undefined
+    				}
+    				trkMsPosition = (trkRefDef.mOffset === undefined) ? msPosition : msPosition + trkRefDef.mOffset;
+    				midiObjectsAndIndex = getMidiObjectsAndIndex(outputTracks, trkRefDef.voiceID, trkMsPosition);
+    				midiObjects = midiObjectsAndIndex.midiObjects;
+    				indexInMidiObjects = midiObjectsAndIndex.index;
+    				trkLength = trkRefDef.length;
+
+    				for(k = 0; k < trkLength; ++k)
+    				{
+    					trk.push(midiObjects[indexInMidiObjects++]);
+    				}
+    				trks.push(trk);
+    			}
+
+    			inputNote.seq = new _AP.seq.Seq(trks);
+
+    			inputNotes.push(inputNote);
+    		}
+
+    		return inputNotes;
+    	}
+
         if(!(this instanceof InputChord))
         {
         	return new InputChord(timeObject, outputTracks);
@@ -33,12 +124,16 @@ _AP.inputChord = (function()
         // The timeObject takes the global speed option into account.
         Object.defineProperty(this, "msPositionInScore", { value: timeObject.msPosition, writable: false });
         Object.defineProperty(this, "msDurationInScore", { value: timeObject.msDuration, writable: false });
+        if(timeObject.inputControls !== undefined)
+        {
+        	Object.defineProperty(this, "inputControls", { value: timeObject.inputControls, writable: false });
+        }
 
     	// an array of inputNote objects. Each inputNote contains the following fields
     	//		inputNote.notatedKey
     	//		inputNote.seq
-    	//		inputNote.inputOptions (can be undefined)
-        this.inputNotes = this.getInputNotes(timeObject.inputChordDef.inputNotes, timeObject.msPosition, outputTracks);
+    	//		inputNote.inputControls (can be undefined)
+        this.inputNotes = getInputNotes(timeObject.inputChordDef.inputNotes, timeObject.msPosition, outputTracks);
 
         return this;
     },
@@ -52,87 +147,7 @@ _AP.inputChord = (function()
     };
     // end var
 
-    InputChord.prototype.getInputNotes = function(inputNoteDefs, msPosition, outputTracks)
-    {
-    	var i, nInputNoteDefs = inputNoteDefs.length, inputNoteDef, 
-			inputNote, inputNotes = [],
-			j, trk, trks, trkMsPosition, trkRefDef, trkRefDefs,
-			trkLength,
-			k, midiObjects, indexInMidiObjects, midiObjectsAndIndex;
-
-    	function getMidiObjectsAndIndex(outputTracks, voiceID, msPosition)
-    	{
-    		var found = false, i, midiObjects, outputTrack, nOutputTracks = outputTracks.length,
-    			index, midiObjectsAndIndex = {};
-
-    		for(i = 0; i < nOutputTracks; ++i)
-    		{
-    			outputTrack = outputTracks[i];
-    			if(outputTrack.voiceID === voiceID)
-    			{
-    				found = true;
-    				break;
-    			}
-    		}
-    		if(found === false)
-    		{
-    			throw "InputChord.js: Can't find the output track with the correct voiceID!";
-    		}
-
-    		found = false;
-    		midiObjects = outputTrack.midiObjects;
-    		for(index = 0; index < midiObjects.length; ++index)
-    		{
-    			if(midiObjects[index].msPositionInScore === msPosition)
-    			{
-    				found = true;
-    				break;
-    			}
-    		}
-    		if(found === false)
-    		{
-    			throw "InputChord.js: Can't find the beginning of the trk!";
-    		}
-
-    		midiObjectsAndIndex.midiObjects = midiObjects;
-    		midiObjectsAndIndex.index = index;
-    		return midiObjectsAndIndex;
-    	}
-
-    	for(i = 0; i < nInputNoteDefs; ++i)
-    	{
-    		inputNoteDef = inputNoteDefs[i];
-    		inputNote = {};
-    		inputNote.notatedKey = inputNoteDef.notatedKey;
-    		trks = [];
-    		inputNote.inputOptions = inputNoteDef.inputOptions; // can be undefined
-    		trkRefDefs = inputNoteDef.trkRefs;
-    		for(j = 0; j < trkRefDefs.length; ++j)
-    		{
-    			trkRefDef = trkRefDefs[j];
-    			trk = [];
-    			trk.msOffset = trkRefDef.mOffset; // can be undefined
-    			trk.inputControls = trkRefDef.inputControls; // can be undefined
-    			trkMsPosition = (trkRefDef.mOffset === undefined) ? msPosition : msPosition + trkRefDef.mOffset;
-    			midiObjectsAndIndex = getMidiObjectsAndIndex(outputTracks, trkRefDef.voiceID, trkMsPosition);
-    			midiObjects = midiObjectsAndIndex.midiObjects;
-    			indexInMidiObjects = midiObjectsAndIndex.index;
-    			trkLength = trkRefDef.length;
-
-    			for(k = 0; k < trkLength; ++k)
-    			{
-    				trk.push(midiObjects[indexInMidiObjects++]);
-    			}
-    			trks.push(trk);
-    		}
-
-    		inputNote.seq = new _AP.seq.Seq(trks);
-
-    		inputNotes.push(inputNote);
-    	}
-
-    	return inputNotes;
-    };
+    // Seq.prototype functions come here...
 
     return publicInputChordAPI;
 }());
