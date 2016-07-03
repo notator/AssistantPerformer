8df1f33e6bf4d4c7c87ca1d43ff8df4d8acda412
notator <j.ingram@netcologne.de>
2016-07-03 10:28:02 +0200

    Bug fix: empty staves now properly ignored
    when setting the running marker's playback
    timeObject list.
    The error was in Markers.js line 429.

--- ap/Markers.js
+++ ap/Markers.js
@@ -426,7 +426,7 @@ _AP.markers = (function ()
                 for (i = 0; i < system.staves.length; ++i)
                 {
                 	staff = system.staves[i];
-                	if(staff.isVisible)
+                	if(staff.isVisible && staff.nameElem !== undefined)
                 	{
                 		for(voiceIndex = 0; voiceIndex < staff.voices.length; ++voiceIndex)
                 		{
